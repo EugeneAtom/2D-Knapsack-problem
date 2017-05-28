45243b76ddd2f10ff3c33e690a5e8aa5419eed44
EugeneAtom <wonderfull.e@mail.ru>
2017-05-28 13:32:52 +0300

    add graphics

--- 2D Knapsack/src/Knapsack/Main.java
+++ 2D Knapsack/src/Knapsack/Main.java
@@ -1,11 +1,38 @@
 package Knapsack;
 
-import Knapsack.Data;
-import Knapsack.GA;
-import Knapsack.Group;
-import Knapsack.InputFromFile;
+import java.awt.*;
+
+import javax.swing.*;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+class MyPaint extends JComponent {
+    int x;
+    int y;
+    int yLength;
+    int xLength;
+    int i;
+    public MyPaint (int i, int x, int y, int xLength, int yLength) {
+        this.x = x;
+        this.y = y;
+        this.yLength = yLength;
+        this.xLength = xLength;
+    }
+
+    public void paint(Graphics g) {
+        Random random = new Random();
+        String c1 = String.valueOf(random.nextInt(8));
+        String c2 = String.valueOf(random.nextInt(7));
+        String c3 = String.valueOf(random.nextInt(2));
+        String c4 = String.valueOf(random.nextInt(6));
+        String c5 = String.valueOf(random.nextInt(5));
+        String c6 = String.valueOf(random.nextInt(1));
+        g.setColor(Color.decode(c1 + c2 + c3 + c4 + c5 + c6));
+        g.fillRect(x, y, xLength, yLength);
+        g.drawRect (x, y, xLength, yLength);
+    }
+}
 
 /**
  * Created by eugene on 5/18/17.
@@ -17,207 +44,41 @@ public class Main {
         String areaFile = "/home/eugene/Documents/2D-Knapsack-problem/2D Knapsack/src/Knapsack/area.txt";
         Rectangle area = InputFromFile.getArea(areaFile);
 
-        int groupSize = 100;
-        int populationSize = 200;
+        int groupSize = 120;
+        int populationSize = 50;
         int square = area.square;
-        int pickedIndividuals = 70;
+        int pickedIndividuals = groupSize / 3 * 2;
 
         Data data = new Data(groupSize, populationSize, square, pickedIndividuals, groupSet.rectangles.size());
 
-        ArrayList<Coordinates> bestGroup = GA.pickBestGroup(data, area, groupSet, 100);
+        ArrayList<Coordinates> bestGroup = GA.pickBestGroup(data, area, groupSet, 40);
         System.out.println("final population: " + bestGroup);
         System.out.println("area square " + area.square);
         double totalSquare = GA.getTotalSquare(bestGroup);
         System.out.println("total square " + totalSquare);
         System.out.println("Percent of filling " + Math.round(totalSquare / area.square * 100) + "%");
-//        for (Coordinates coordinates : bestGroup) {
-//            Polygon p = new Polygon();
-//            p.addPoint(coordinates.A.x, coordinates.A.y);
-//            p.addPoint(coordinates.B.x, coordinates.B.y);
-//            p.addPoint(coordinates.C.x, coordinates.C.y);
-//            p.addPoint(coordinates.D.x, coordinates.D.y);
-//            Graphics g = new Graphics() {
-//                @Override
-//                public Graphics create() {
-//                    return null;
-//                }
-//
-//                @Override
-//                public void translate(int x, int y) {
-//
-//                }
-//
-//                @Override
-//                public Color getColor() {
-//                    return null;
-//                }
-//
-//                @Override
-//                public void setColor(Color c) {
-//
-//                }
-//
-//                @Override
-//                public void setPaintMode() {
-//
-//                }
-//
-//                @Override
-//                public void setXORMode(Color c1) {
-//
-//                }
-//
-//                @Override
-//                public Font getFont() {
-//                    return null;
-//                }
-//
-//                @Override
-//                public void setFont(Font font) {
-//
-//                }
-//
-//                @Override
-//                public FontMetrics getFontMetrics(Font f) {
-//                    return null;
-//                }
-//
-//                @Override
-//                public Knapsack.Rectangle getClipBounds() {
-//                    return null;
-//                }
-//
-//                @Override
-//                public void clipRect(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public void setClip(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public Shape getClip() {
-//                    return null;
-//                }
-//
-//                @Override
-//                public void setClip(Shape clip) {
-//
-//                }
-//
-//                @Override
-//                public void copyArea(int x, int y, int width, int height, int dx, int dy) {
-//
-//                }
-//
-//                @Override
-//                public void drawLine(int x1, int y1, int x2, int y2) {
-//
-//                }
-//
-//                @Override
-//                public void fillRect(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public void clearRect(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-//
-//                }
-//
-//                @Override
-//                public void fillRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-//
-//                }
-//
-//                @Override
-//                public void drawOval(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public void fillOval(int x, int y, int width, int height) {
-//
-//                }
-//
-//                @Override
-//                public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-//
-//                }
-//
-//                @Override
-//                public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-//
-//                }
-//
-//                @Override
-//                public void drawPolyline(int[] xPoints, int[] yPoints, int nPoints) {
-//
-//                }
-//
-//                @Override
-//                public void drawPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-//
-//                }
-//
-//                @Override
-//                public void fillPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-//
-//                }
-//
-//                @Override
-//                public void drawString(String str, int x, int y) {
-//
-//                }
-//
-//                @Override
-//                public void drawString(AttributedCharacterIterator iterator, int x, int y) {
-//
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int x, int y, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int x, int y, int width, int height, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int x, int y, Color bgcolor, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int x, int y, int width, int height, Color bgcolor, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, Color bgcolor, ImageObserver observer) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public void dispose() {
-//
-//                }
-//            };
-//            g.drawPolygon(p);
-//        }
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, area.xLength, area.yLength);
+        int i = 0;
+        for (Coordinates rectangle : bestGroup) {
+            window.getContentPane().add(new MyPaint(i, rectangle.A.x, rectangle.A.y, rectangle.xLength, rectangle.yLength));
+            window.setVisible(true);
+            i += 1;
+        }
+
+//        JFrame window = new JFrame();
+//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        window.setBounds(100, 100, area.xLength, area.yLength);
+//        window.getContentPane().add(new MyPaint(10, 100, 100, 200, 200));
+//        window.setVisible(true);
+//        window.getContentPane().add(new MyPaint(10, 300, 100, 200, 200));
+//        window.setVisible(true);
+//        window.getContentPane().add(new MyPaint(10, 100, 300, 200, 200));
+//        window.setVisible(true);
+//        window.getContentPane().add(new MyPaint(10, 300, 300, 200, 200));
+//        window.getContentPane().add(new MyPaint(10, 0, 00, 20, 20));
+        window.setVisible(true);
     }
 }
